312f2f095a3218aad8b6133319f6e678145aec2d
aatosmedia <tommi.saksa@aatosmedia.fi>
2024-04-04 11:13:25 +0300

    initial commit

---
+++ routes/products.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const productController = require('../controllers/productController');
+
+router.get('/', productController.getHome)
+router.get('/products/:id', productController.getProductDetails)
+
+module.exports = router;
